2b366bedef468c06c7f27043afd3a7b15dff2762
Timothy <timothyolofson@gmail.com>
2024-10-29 00:04:26 -0700

    Finished first lesson on Promises via Javascript. (resubmission)

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
-// const statusRef = document.querySelector(".email");
-const statusRef = document.querySelector(".status");
+// Timothy Olofson
+// Frontend Simplified(FES)
+// Week 3: Javascript
+// Lesson: Promises
+
+console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
+
+const emailRef = document.querySelector(".email");
 const videoRef = document.querySelector(".video");
+const statusRef = document.querySelector(".status");
+
+
+// Then 
+
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+
+//   .then((response) => {
+
+//     return response.json();
+//   })
+
+//   .then((data) => {
+
+//     emailRef.innerHTML = data.email;
+//   })
+
+
+// Async/Await
+
+// async function main() {
+  
+//   const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+
+//   const data = await response.json();
+
+//   emailRef.innerHTML = data.email;
+// }
+
 
 function getSubscriptionStatus() {
 
@@ -12,6 +47,7 @@ function getSubscriptionStatus() {
     }, 2000);
   });
 }
+
 
 function getVideo(SubscriptionStatus) {
 
@@ -31,6 +67,7 @@ function getVideo(SubscriptionStatus) {
     }
   });
 }
+
 
 async function main() {
 
